514be5d737a3a7de4ec415126c6fc72a535d7770
Rafa Alvargonzalez <isildur4@hotmail.com>
2026-04-22 15:04:06 +0200

    Actualizacion final de las practicas de condicionales

--- js/main.js
+++ js/main.js
@@ -1,36 +1,52 @@
-/*
-function mayorDeDos(n1, n2) {
-    if (n1 == n2) {
-        return "Son iguales";
-    } else if (n1 > n2) {
-        return n1;
+//positivo, negativo o 0
+function determinarSigno(num) {
+    if (num > 0) {
+        return "positivo";
+    } else if (num < 0) {
+        return "negativo";
     } else {
-        return n2;
+        return "cero";
     }
 }
-mayorDeDos(5, 10);
-mayorDeDos(20, 15);
-mayorDeDos(7, 7);
+console.log("El número 5 es " + determinarSigno(5));
+console.log("El número -3 es " + determinarSigno(-3));
+console.log("El número 0 es " + determinarSigno(0));
 
 
+//comprobar mayoria de edad
+function puedeVotar(edad) {
+    return (edad >= 18) ? "puede votar" : ("no pude votar");
+}
+for (let i = 15; i < 25; i++)
+    console.log(`Con ${i} años: ${puedeVotar(i)}`)
+
+//mayor de 2 numeros
+function mayorDeDos(n1, n2) {
+    return Math.max(n1, n2);
+}
+console.log("El mayor de 5 y 10 es " + mayorDeDos(5, 10));
+console.log("El mayor de 20 y 15 es " + mayorDeDos(20, 15));
+console.log("El mayor de 77 y 77 es " + mayorDeDos(77, 77));
+
+
+//calcular multiplos de 5
 function esDivisiblePorCinco(n) {
     return (n % 5 == 0)
 }
-console.log("¿10 es multiplo de 5?" + (esDivisiblePorCinco(5) ? "Sí" : "No"));
-console.log("¿10 es multiplo de 5?" + (esDivisiblePorCinco(-5) ? "Sí" : "No"));
-console.log("¿10 es multiplo de 5?" + (esDivisiblePorCinco(0) ? "Sí" : "No"));
-console.log("¿10 es multiplo de 5?" + (esDivisiblePorCinco(23) ? "Sí" : "No"));
-*/
+console.log("¿10 es multiplo de 5? " + (esDivisiblePorCinco(5) ? "Sí" : "No"));
+console.log("¿10 es multiplo de 5? " + (esDivisiblePorCinco(-5) ? "Sí" : "No"));
+console.log("¿10 es multiplo de 5? " + (esDivisiblePorCinco(0) ? "Sí" : "No"));
+console.log("¿10 es multiplo de 5? " + (esDivisiblePorCinco(23) ? "Sí" : "No"));
 
+
+//asignar una nota en funcion del puntaje
 function clasificarNota(num) {
-    if (num >= 90) {
-        return "Excelente"
-    } else if (num >= 70) {
-        return "Buena"
-    } else if (num >= 50) {
-        return "Regular"
-    } else
-        return "Insuficiente"
+    function clasificarNota(num) {
+        if (num >= 90) return "Excelente";
+        if (num >= 70) return "Buena";
+        if (num >= 50) return "Regular";
+        return "Insuficiente";
+    }
 }
 
 for (let i = 0; i <= 10; i++) {
